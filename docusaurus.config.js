b0a2f299c1819293e1a55c906b1bd87ee7858c6c
Max Andreev <maxick20@gmail.com>
2025-04-21 20:20:32 +0500

    change baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://nftmozaic.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
