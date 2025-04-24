cb9a81a8eed98aae8e30c6390998ccb8e03e4532
FFilipUnique <ff@unique.network>
2025-04-24 10:54:09 +0200

    Set styling as per brand guide

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,125 +10,125 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'NFTMozaic',
-  tagline: 'The Polkadot NFT Alliance',
-  favicon: 'img/favicon.ico',
+	title: 'NFTMozaic',
+	tagline: 'The Polkadot NFT Alliance',
+	favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://nftmozaic.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Wiki/',
+	// Set the production url of your site here
+	url: 'https://nftmozaic.com',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/Wiki/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nftmozaic', // Usually your GitHub org/user name.
-  projectName: 'nftmozaic-wiki', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'nftmozaic', // Usually your GitHub org/user name.
+	projectName: 'nftmozaic-wiki', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: './sidebars.js',
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ['rss', 'atom'],
+						xslt: true,
+					},
+					// Useful options to enforce blogging best practices
+					onInlineTags: 'warn',
+					onInlineAuthors: 'warn',
+					onUntruncatedBlogPosts: 'warn',
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'NFTMozaic',
-        logo: {
-          alt: 'NFTMozaidc Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Polkadot NFTs',
-          },
-          //{ to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} NFTMozaic. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			stylesheets: [
+				'/css/custom.css',
+			],
+			// Replace with your project's social card
+			image: 'img/docusaurus-social-card.jpg',
+			navbar: {
+				title: 'NFTMozaic',
+				logo: {
+					alt: 'NFTMozaidc Logo',
+					src: 'img/logo.svg',
+				},
+				items: [
+					{
+						type: 'docSidebar',
+						sidebarId: 'tutorialSidebar',
+						position: 'left',
+						label: 'Polkadot NFTs',
+					},
+					//{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						href: 'https://github.com/facebook/docusaurus',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Stack Overflow',
+								href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+							},
+							{
+								label: 'Discord',
+								href: 'https://discordapp.com/invite/docusaurus',
+							},
+							{
+								label: 'X',
+								href: 'https://x.com/docusaurus',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'Blog',
+								to: '/blog',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/facebook/docusaurus',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} NFTMozaic. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: prismThemes.github,
+				darkTheme: prismThemes.dracula,
+			},
+		}),
 };
 
 export default config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,51 @@
+import React from 'react';
 import clsx from 'clsx';
+import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import { useColorMode } from '@docusaurus/theme-common';
+// **** ADD useBaseUrl back ****
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+	const { siteConfig } = useDocusaurusContext();
+	const { colorMode } = useColorMode();
 
+	// Define logo paths (relative to the 'static' folder)
+	const logoLight = '/img/NFTMozaic-pos.svg';
+	const logoDark = '/img/NFTMozaic-neg.svg';
+
+	// **** Use useBaseUrl ****
+	const logoSrc = useBaseUrl(colorMode === 'dark' ? logoDark : logoLight);
+
+	return (
+		<header className={clsx('hero hero--primary', styles.heroBanner, styles.splashContainer)}>
+			<div className="container">
+				<img
+					className={styles.splashLogo}
+					// **** Use the variable with useBaseUrl ****
+					src={logoSrc}
+					alt={`${siteConfig.title} Logo`}
+				/>
+				<p className={styles.splashText}>
+					NFTMozaic is an alliance within the Polkadot ecosystem dedicated to advancing NFT growth, adoption, and interoperability. It connects parachains, projects, and partners to foster collaboration, drive technical innovation, and support real-world applications to position Polkadot as a leader in next-gen NFTs.
+				</p>
+			</div>
+		</header>
+	);
+}
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={"Welcome"}
-      //title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={`Home`} // Or keep the default Hello from...
+			description="NFTMozaic: Advancing NFT growth, adoption, and interoperability within the Polkadot ecosystem."> {/* Update description */}
+			<HomepageHeader />
+			<main>
+				{/* --- Remove or Comment Out the HomepageFeatures section --- */}
+				{/* <HomepageFeatures /> */}
+			</main>
+		</Layout>
+	);
 }
